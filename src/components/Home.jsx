740e1d85ca2df599a2abe4e3b46110e0f05d66e6
Pau Mesonero <paumesonero@Paus-Mac-mini.local>
2022-08-26 13:48:04 +0200

    iteration 1 done

---
+++ src/components/Home.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import beersImg from "../assets/beers.png"
+import randomBeerImg from "../assets/random-beer.png"
+import newBeerImg from "../assets/new-beer.png"
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <img src={beersImg} alt="beers background" height="150px" />
+        <Link to="/beers"> All Beers</Link>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Facere, fuga repellendus error ullam labore aspernatur hic
+          laborum iure culpa, harum blanditiis enim laboriosam tempora
+          maiores dignissimos iste optio assumenda necessitatibus.</p>
+      </div>
+
+      <div>
+        <img src={randomBeerImg} alt="random beer background" height="150px" />
+        <Link to="/random-beer">Random Beer</Link>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Facere, fuga repellendus error ullam labore aspernatur hic
+          laborum iure culpa, harum blanditiis enim laboriosam tempora
+          maiores dignissimos iste optio assumenda necessitatibus.</p>
+      </div>
+
+      <div>
+        <img src={newBeerImg} alt="new beer background" height="150px" />
+        <Link to="/new-beer">New Beer</Link>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Facere, fuga repellendus error ullam labore aspernatur hic
+          laborum iure culpa, harum blanditiis enim laboriosam tempora
+          maiores dignissimos iste optio assumenda necessitatibus.</p>
+      </div>
+
+    </div>
+  )
+}
